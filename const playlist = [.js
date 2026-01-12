7df65870b1fc84ad5b4511364659baa96c505ec2
Ingrid <ingrid.batista.b.2002@gmail.com>
2026-01-12 18:18:41 -0300

    Add files via upload

---
+++ const playlist = [.js
@@ -0,0 +1,16 @@
+const playlist = [
+    "musicas/Renai Circulation.mp4",
+    "musicas/Super Bright Shining Girl _Chika_.mp4",
+    "musicas/Fiction   Sumika.mp4",
+    "musicas/Kawaki wo Ameku.mp4",
+    "musicas/Hikaru Nara - Goose House.mp4",
+    "musicas/The Cruel Angel's Thesis _ Evangelion Finally.mp4",
+    "musicas/86 EIGHTY-SIX - Ending Full『Avid』by SawanoHiroyuki[nZk]_mizuki.mp4",
+    "musicas/Kenshi Yonezu - Jane Doe (Lyrics).mp4",
+    "musicas/YOASOBI – '怪物 (Monster)' Lyrics.mp4",
+    "musicas/MEZAME RETURNER.mp4",
+    "musicas/Clattanoia Music Clip.mp4",
+    "musicas/Zom 100_ Bucket List of the Dead.mp4",
+    "musicas/ODD FUTURE.mp4",
+    "musicas/Mrs. GREEN APPLE（Inferno）.mp4"
+];
